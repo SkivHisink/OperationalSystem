923ce4f4b21975e9f24e68395512cbc66d428478
SkivHisink <skiv27rus@gmail.com>
2020-09-25 06:04:15 +0700

    commit

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "pushing files..."
-git add .
-git commit -m "commit"
-echo "successful commited"
-sleep 1 
-echo "pushing origin"
-git push -u origin master
--- task3/main.c
+++ task3/main.c
@@ -1,5 +1,29 @@
-
-
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+int write_uid_and_euid()
+{
+	printf("The real user ID : %d\nThe effective user ID : %d\n",
+	 getuid(), geteuid());
+	FILE* file = fopen("file", "r");
+	if(file==NULL)
+	{
+		perror("File did't open");
+	}
+	else
+	{
+	//fprintf(file, "%s %s\n", getuid(), geteuid());
+	fclose(file);
+	printf("File opened\n");
+	return 0;
+	}
+}
 int main()
 {
+	if(write_uid_and_euid()==0)
+	{
+	setuid(geteuid());
+	write_uid_and_euid();
+	}
+	return 0;
 }
