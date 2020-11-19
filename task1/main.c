66819eb10d04bf063268c9a0b44fba4e4b27280f
SkivHisink <49669372+SkivHisink@users.noreply.github.com>
2020-11-20 09:31:35 +1000

    function in main

--- task1/main.c
+++ task1/main.c
@@ -1,6 +1,41 @@
-#include <stdio.h>
 
-int main()
-{
-
+int main(int argc, char* argv[]) {
+    int param;
+    while ((param = getopt(argc, argv, "ispuU:cC:dvV:")) != -1) {
+        switch (param) {
+        case 'i':
+            print_gu_ids();
+            break;
+        case 's':
+            make_group_owner();
+            break;
+        case 'p':
+            print_p_ids();
+            break;
+        case 'u':
+            print_ulimit();
+            break;
+        case 'U':
+            set_ulimit(optarg);
+            break;
+        case 'c':
+            print_core_limit();
+            break;
+        case 'C':
+            set_core_limit(optarg);
+            break;
+        case 'd':
+            print_dir();
+            break;
+        case 'v':
+            print_env();
+            break;
+        case 'V':
+            put_env(optarg);
+            break;
+        default: 
+            return 1;
+        }
+    }
+    return 0;
 }
