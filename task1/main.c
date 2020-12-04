85546e0ff8a02a16167c959e6d4214bb3650dea5
SkivHisink <49669372+SkivHisink@users.noreply.github.com>
2020-12-04 11:59:08 +1000

    Update main.c

--- task1/main.c
+++ task1/main.c
@@ -1,23 +1,10 @@
+#include "functions.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     int param;
     while ((param = getopt(argc, argv, "ispuU:cC:dvV:")) != -1) {
         switch (param) {
-        case 'i':
-            print_gu_ids();
-            break;
-        case 's':
-            make_group_owner();
-            break;
-        case 'p':
-            print_p_ids();
-            break;
-        case 'u':
-            print_ulimit();
-            break;
-        case 'U':
-            set_ulimit(optarg);
-            break;
         case 'c':
             print_core_limit();
             break;
@@ -26,6 +13,21 @@ int main(int argc, char* argv[]) {
             break;
         case 'd':
             print_dir();
+            break;
+        case 'i':
+            print_gu_ids();
+            break;
+        case 'p':
+            print_p_ids();
+            break;
+        case 's':
+            make_group_owner();
+            break;
+        case 'u':
+            print_ulimit();
+            break;
+        case 'U':
+            set_ulimit(optarg);
             break;
         case 'v':
             print_env();
