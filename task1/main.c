e01b7d2f83768cd5a8008fba66709f5be2742c9c
SkivHisink <49669372+SkivHisink@users.noreply.github.com>
2020-12-11 09:58:15 +1000

    Update main.c

--- task1/main.c
+++ task1/main.c
@@ -1,4 +1,26 @@
-#include "functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/resource.h>
+
+#define MAXDIR 256
+
+extern char **environ;
+
+void make_group_owner();
+
+void print_core_limit();
+void print_dir();
+void print_env();
+void print_p_ids();
+void print_ulimit();
+
+void set_core_limit(char *arg);
+void set_ulimit(char *arg);
+
+void put_env(char *arg);
 
 int main(int argc, char* argv[])
 {
@@ -40,4 +62,104 @@ int main(int argc, char* argv[])
         }
     }
     return 0;
+}
+
+void make_group_owner() {
+    if (setpgid(0, 0))
+        perror("setpgid");
+}
+
+void print_core_limit() {
+    struct rlimit limit;
+    if (getrlimit(RLIMIT_CORE, &limit) == 0) {
+        printf("Core file size limit: %ld\n", limit.rlim_cur);
+    } else {
+        perror("getrlimit");
+    }
+}
+
+void print_dir() {
+    char buffer[MAXDIR];
+    char *dir = getcwd(buffer, MAXDIR);
+    if (dir) {
+        printf("%s\n", dir);
+    } else {
+        perror("getcwd");
+    }
+}
+
+void print_gu_ids() {
+    printf("Real user ID: %d\n", getuid());
+    printf("Effective user ID: %d\n", geteuid());
+    printf("Real group ID: %d\n", getgid());
+    printf("Effective group ID: %d\n", getegid());
+}
+
+void print_env() {
+    for (char **env = environ; *env; ++env)
+        printf("%s\n", *env);
+}
+
+void print_p_ids() {
+    printf("Process ID: %d\n", getpid());
+    printf("Parent process ID: %d\n", getppid());
+    printf("Process group ID: %d\n", getpgrp());
+}
+
+void print_ulimit() {
+    struct rlimit limit;
+    if (getrlimit(RLIMIT_FSIZE, &limit) == 0) {
+        if (limit.rlim_cur == RLIM_INFINITY) {
+            printf("Ulimit: unlimited\n");
+        } else {
+            printf("Ulimit: %ld\n", limit.rlim_cur);
+        }
+    } else {
+        perror("getrlimit");
+    }
+}
+
+void set_core_limit(char *arg) {
+    char *endptr = NULL;
+    errno = 0;
+    long newLimit = strtol(arg, &endptr, 10);
+    if (errno != 0 || *endptr || newLimit < 0) {
+        fprintf(stderr, "Invalid new core limit value: %s\n", arg);
+        return;
+    }
+
+    struct rlimit limit;
+    if (getrlimit(RLIMIT_CORE, &limit) == 0) {
+        limit.rlim_cur = (rlim_t) newLimit;
+        if (setrlimit(RLIMIT_CORE, &limit) != 0) {
+            perror("setrlimit");
+        }
+    } else {
+        perror("getrlimit");
+    }
+}
+
+voidset_ulimit(char *arg) {
+    char *endptr = NULL;
+    errno = 0;
+    long newLimit = strtol(arg, &endptr, 10);
+    if (errno != 0 || *endptr || newLimit < 0) {
+        fprintf(stderr, "Invalid new limit value: %s\n", arg);
+        return;
+    }
+
+    struct rlimit limit;
+    if (getrlimit(RLIMIT_FSIZE, &limit) == 0) {
+        limit.rlim_cur = (rlim_t) newLimit;
+        if (setrlimit(RLIMIT_FSIZE, &limit) != 0) {
+            perror("setrlimit");
+        }
+    } else {
+        perror("getrlimit");
+    }
+}
+
+void put_env(char *arg) {
+    if (putenv(arg) != 0)
+        perror("putenv");
 }
